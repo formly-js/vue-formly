9450dbf0358f7dfe110c9c379793b4e05898998b
Matt Sanders <matt.d.s.sanders@gmail.com>
2016-08-23 17:13:27 +1200

    fixed the async action so it doesn't generate extra webpack files

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,14 @@
 import FormlyForm from './FormlyForm.vue';
+import FormlyField from './FormlyField.vue';
 
 export default function(Vue){
     Vue.component('formly-form', FormlyForm);
+    
     Vue.component('formly-field', (resolve) => {
         /**
          * FormlyField must be loaded asyncronously so that any fields added in 
          * via Formly.addType are available
          */
-        require(['./FormlyField.vue'], resolve);
+        resolve(FormlyField);
     });
 }
