39fdd46f09fb0dc0eab33432aa04e17338be68a6
Matt Sanders <matt.d.s.sanders@gmail.com>
2016-12-12 07:28:26 +1300

    working on the FormlyField component

--- test/unit/specs/FormlyField.spec.js
+++ test/unit/specs/FormlyField.spec.js
@@ -6,199 +6,219 @@ import FormlyField from 'src/components/FormlyField.vue';
 let el, vm;
 
 function createForm(template, data){
-    el = document.createElement('div');
+  el = document.createElement('div');
   //el.innerHTML = template;
-    vm = new Vue({
-        el: el,
-      data: data,
-      template: template,
-        components: {
-            'formly-field': FormlyField
-        }
-    });
+  vm = new Vue({
+    el: el,
+    data: data,
+    template: template,
+    components: {
+      'formly-field': FormlyField
+    }
+  }).$mount();
 
-    return [el, vm];
+  return [el, vm];
 }
 
-/*
+
 describe('FormlyField', () => {
 
-    it('should take on the type of another component', () => {
+  it('should take on the type of another component', () => {
 
-        Vue.component('formly_test', {
-            props: ['form', 'key'],
-            template: '<div id="testComponent">{{form[key].type}}</div>'
-        });
-
-        let data = {
-            form:{
-                $errors: {},
-                $valid: {},
-                test: {
-                    type: 'test'
-                }
-            }
-        };
-        
-        createForm('<formly-field :form="form" key="test"></formly-field>', data);
-
-        let innerElem = vm.$el.querySelector('#testComponent');
-
-        expect(innerElem.textContent).to.contain(data.form.test.type);
-        
+    Vue.component('formly_test', {
+      props: ['form', 'model', 'field'],
+      template: '<div id="testComponent">{{field.type}}</div>'
     });
 
- 
-    it('should mimic the model of the parent', (done) => {
+    let data = {
+      form:{
+        $errors: {},
+        $valid: {},
+      },
+      fields: [
+        {
+          key: 'test',
+          type: 'test' 
+        }
+      ],
+      model: {
+        test: ''
+      }
+    };
+    
+    createForm('<formly-field :form="form" :model="model" :field="fields[0]"></formly-field>', data);
 
-        Vue.component('formly_test', {
-            props: ['form', 'key'],
-            template: '<input type="text" id="testInput" v-model="form[key].value">'
-        });
+    expect(vm.$el.textContent).to.equal(data.fields[0].type);
+    
+  });
 
-        let data = {
-            form: {
-                $errors: {},
-                $valid: {},
-                search: {
-                    type: 'test',
-                    value: 'foo'
-                }
-            }
-        };
+  
+  it('should mimic the model of the parent', (done) => {
 
-        createForm('<formly-field :form.sync="form" key="search"></formly-field>', data);
-
-        let input = vm.$el.querySelector('#testInput');
-
-        expect(input.value).to.contain('foo');
-
-        //change the value and expect a change
-        vm.form.search.value = 'bar';
-
-        setTimeout(() => {
-            expect(input.value).to.equal('bar');
-            done();
-        }, 0);
-        
+    Vue.component('formly_test', {
+      props: ['form', 'field', 'model'],
+      template: '<input type="text" id="testInput" v-model="model[ field.key ]">'
     });
 
-    describe('Validation', ()=>{
+    let data = {
+      form: {
+        $errors: {},
+        $valid: {}
+      },
+      fields: [
+        {
+          key: 'search',
+          type: 'test' 
+        }
+      ],
+      model: {
+        search: 'foo'
+      }
+    };
 
-        before(()=>{
-            Vue.component('formly_test', {
-                props: ['form', 'key'],
-                template: '<div></div>'
-            });
-        });
+    createForm('<formly-field :form.sync="form" :field="fields[0]" :model="model"></formly-field>', data);
 
-        function createValidField(data){
-            return createForm('<formly-field :form.sync="form" key="search"></formly-field>', data);
-        };
+    expect(vm.$el.value).to.equal('foo');
 
-        it('should handle required values', (done) => {
+    //change the value and expect a change
+    vm.model.search = 'bar';
 
-            let data = {
-                form: {
-                    $valid: true,
-                    $errors: {},
-                    search: {
-                        type: 'test',
-                        value: '',
-                        required: true
-                    }
-                }
-            };
-            
-            createValidField(data);
-            expect(vm.form.$errors.search.required).to.be.true;
+    setTimeout(() => {
+      expect(vm.$el.value).to.equal('bar');
+      done();
+    }, 0);
+    
+  });
 
-            vm.$set('form.search.value','testing');
-            setTimeout(()=>{
-                expect(vm.form.$errors.search.required).to.be.false;
-                done();
-            },0);
-            
-        });
+  
+  describe('Validation', ()=>{
 
-        it('should take an expression', (done) => {
-            let data = {
-                form: {
-                    $valid: true,
-                    $errors: {},
-                    search: {
-                        type: 'test',
-                        value: 'testing',
-                        validators: {
-                            expression: 'field.value == "test"'
-                        }
-                    }
-                }
-            };
+    before(()=>{
+      Vue.component('formly_test', {
+        props: ['form', 'field', 'model'],
+        template: '<div></div>'
+      });
+    });
 
-            createValidField(data);
-            expect(vm.form.$errors.search.expression).to.be.true;
+    function createValidField(data){
+      return createForm('<formly-field :form.sync="form" :field="fields[0]" :model="model"></formly-field>', data);
+    };
 
-            vm.$set('form.search.value', 'test');
-            setTimeout(()=>{
-                expect(vm.form.$errors.search.expression).to.be.false;
-                done();
-            },0);
-        });
+    it('should handle required values', (done) => {
 
-        it('should not require non-required values', (done) => {
-            let data = {
-                form: {
-                    $valid: true,
-                    $errors: {},
-                    search: {
-                        type: 'test',
-                        value: '',
-                        validators: {
-                            expression: 'field.value == "test"'
-                        }
-                    }
-                }
-            };
+      let data = {
+        form: {
+          $valid: true,
+          $errors: {}
+        },
+        model: {
+          search: ''
+        },
+        fields: [
+          {
+            key: 'search',
+            type: 'test',
+            required: true
+          }
+        ]
+      };
+      
+      createValidField(data);
+      expect(vm.form.$errors.search.required).to.be.true;
+      vm.model.search = 'testing';
+      setTimeout(()=>{
+        expect(vm.form.$errors.search.required).to.be.false;
+        done();
+      },0);
+      
+    });
 
-            createValidField(data);
-            expect(vm.form.$errors.search.expression).to.be.false;
+    it('should take an expression', (done) => {
+      let data = {
+        form: {
+          $valid: true,
+          $errors: {}
+        },
+        model: {
+          search: 'testing'
+        },
+        fields: [
+          {
+            key: 'search',
+            type: 'test',
+            validators: {
+              expression: 'model.search == "test"'
+            }
+          }
+        ]
+      };
 
-            vm.$set('form.search.value', 'testing');
-            setTimeout(()=>{
-                expect(vm.form.$errors.search.expression).to.be.true;
-                done();
-            },0);
-        });
-
-        it('should take a function', (done) => {
-            let data = {
-                form: {
-                    $valid: true,
-                    $errors: {},
-                    search: {
-                        type: 'test',
-                        value: 'testing',
-                        validators: {
-                            expression: function(field){
-                                return field.value == 'test';
-                            }
-                        }
-                    }
-                }
-            };
-
-            createValidField(data);
-            expect(vm.form.$errors.search.expression).to.be.true;
-
-            vm.$set('form.search.value', 'test');
-            setTimeout(()=>{
-                expect(vm.form.$errors.search.expression).to.be.false;
-                done();
-            },0);
-        });
-        
+      createValidField(data);
+      expect(vm.form.$errors.search.expression).to.be.true;
+      vm.model.search = 'test';
+      setTimeout(()=>{
+        expect(vm.form.$errors.search.expression).to.be.false;
+        done();
+      },0);
     });
     
+  });
+
+  /*
+
+    it('should not require non-required values', (done) => {
+      let data = {
+        form: {
+          $valid: true,
+          $errors: {},
+          search: {
+            type: 'test',
+            value: '',
+            validators: {
+              expression: 'field.value == "test"'
+            }
+          }
+        }
+      };
+
+      createValidField(data);
+      expect(vm.form.$errors.search.expression).to.be.false;
+
+      vm.$set('form.search.value', 'testing');
+      setTimeout(()=>{
+        expect(vm.form.$errors.search.expression).to.be.true;
+        done();
+      },0);
+    });
+
+    it('should take a function', (done) => {
+      let data = {
+        form: {
+          $valid: true,
+          $errors: {},
+          search: {
+            type: 'test',
+            value: 'testing',
+            validators: {
+              expression: function(field){
+                return field.value == 'test';
+              }
+            }
+          }
+        }
+      };
+
+      createValidField(data);
+      expect(vm.form.$errors.search.expression).to.be.true;
+
+      vm.$set('form.search.value', 'test');
+      setTimeout(()=>{
+        expect(vm.form.$errors.search.expression).to.be.false;
+        done();
+      },0);
+    });
+    
+  });
+  */    
 });
-*/
+
